80579aab5241dd76ab91f8c9ee3b6eaaea671061
Yilin20021016 <werock2517@gmail.com>
2023-11-30 04:16:01 +0800

    [fix]Fix exceptions in training code

--- YOLOv8_predict.py
+++ YOLOv8_predict.py
@@ -1,30 +1,52 @@
+# ~/anaconda3/envs/Torch python3.9.18
+# -*- coding: utf-8 -*-
+
 from ultralytics import YOLO
 import cv2
+from YOLOv8_train import find_FinalVersion
 
-model = YOLO('models/G1.pt')
-result = model.predict(source='./dataset/test/images/IMG_2119.jpg', save=True)
-
-
-# camera = cv2.VideoCapture(0)
-# if not camera.isOpened():
-#     print('camera not found')
-#     exit()
-
-# while True:
-#     success, frame = camera.read()
-#     if not success:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-
-#     result = model.predict(source=frame)
-#     annotated_frame = result[0].plot()
+if __name__ == '__main__':
     
+    try: # 尋找版本
+        version  = find_FinalVersion('train')
+        print(f'目前使用第{version}代測試')
 
+    except FileNotFoundError: # 訓練第一代
+        print('請先訓練第一代模型')
+        exit(0)
 
-#     cv2.imshow('test', annotated_frame)
+    except Exception as e:  # 例外狀況
+        print(e)
 
-#     if cv2.waitKey(1) == ord('q'):
-#         break
+    else:
+        model = YOLO(f'models/G{version}/best.pt')
+        method = input('要以哪種方式測試?\n1.圖片或影片\n2.實時鏡頭\n')
+        if method == '1':  # 以圖片或影片測試
+            target = input('請貼上測試檔案的完整路徑(須為jpg、mp4): ')
+            result = model.predict(source=f'{target}', save=True)
+        elif method == '2':  # 鏡頭實時測試
+            camera = cv2.VideoCapture(0)
+            if not camera.isOpened():
+                print('camera not found')
+                exit()
 
-# camera.release()
-# cv2.destroyAllWindows()
+            while True:
+                success, frame = camera.read()
+                if not success:
+                    print("Can't receive frame (stream end?). Exiting ...")
+                    break
+
+                result = model.predict(source=frame)
+                annotated_frame = result[0].plot()
+                infoText = "Press q to exit"
+                cv2.putText(annotated_frame, infoText, (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 1)
+                cv2.imshow('test', annotated_frame)
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+            camera.release()
+            cv2.destroyAllWindows()
+else:
+    print('error')
+exit()
--- YOLOv8_train.py
+++ YOLOv8_train.py
@@ -1,5 +1,5 @@
 # ~/anaconda3/envs/Torch python3.9.18
-# -*- coding: uft-8 -*-
+# -*- coding: utf-8 -*-
 
 from ultralytics import YOLO
 from shutil import copy
@@ -14,27 +14,12 @@ def find_FinalVersion(set:str) -> str:
 
 if __name__ == '__main__':
 
-    try: # 訓練新一代
+    try: # 尋找版本
         version  = find_FinalVersion('train')
-        if version == '':
-            version = '1'
-        print(f'目前訓練為第{version+1}代')
 
-        model = YOLO(f'./models/G{version}/last.pt')
-        results = model.train(data="dataset/data.yaml", epochs=300, batch=3, workers=0)
-        results = model.val(data="dataset/data.yaml")
-
-        if not path.exists(f'./models/G{version+1}'):
-            mkdir(f'./models/G{version+1}')
-        copy(f'./runs/detect/train{version+1}/weights/best.pt',
-             f'./models/G{version+1}')
-        copy(f'./runs/detect/train{version+1}/weights/last.pt',
-             f'./models/G{version+1}')
-        
-        exit(0)
-    except FileExistsError: # 訓練第一代
+    except FileNotFoundError: # 訓練第一代
         model = YOLO('./models/yolov8n.pt')
-        results = model.train(data="dataset/data.yaml", epochs=300, batch=3, workers=0)
+        results = model.train(data="dataset/data.yaml", epochs=300, batch=3)
         results = model.val(data="dataset/data.yaml")
 
         if not path.exists(f'./models/G1'):
@@ -44,6 +29,26 @@ if __name__ == '__main__':
         copy(f'./runs/detect/train/weights/last.pt',
              f'./models/G1')
         
-        exit(0)
-    # except:
-    #     print('請檢察路徑是否正確')
+    except Exception as e:  # 例外狀況
+        print(e)
+
+    else: # 訓練新一代
+        if version == '':
+            version = 1
+        else:
+            version = int(version)
+        print(f'目前訓練為第{version+1}代')
+
+        model = YOLO(f'./models/G{version}/last.pt')
+        results = model.train(data="dataset/data.yaml", epochs=300, batch=3)
+        results = model.val(data="dataset/data.yaml")
+
+        if not path.exists(f'./models/G{version+1}'):
+            mkdir(f'./models/G{version+1}')
+        copy(f'./runs/detect/train{version+1}/weights/best.pt',
+             f'./models/G{version+1}')
+        copy(f'./runs/detect/train{version+1}/weights/last.pt',
+             f'./models/G{version+1}')
+        
+    finally:
+        exit()
